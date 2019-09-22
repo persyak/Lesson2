91def8d5c908f51ed1f6800e72f3d60c7dbc0782
Oleksandr Ohorodnik <persyak@ukr.net>
2019-09-22 12:58:11 +0300

    Completed lesson 2 task using methods

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -27,59 +27,36 @@ namespace Lesson2
             Console.WriteLine("What's your name?");
             string personName = Console.ReadLine();
 
-            Console.WriteLine("Your gender?");
-            string personGender = Console.ReadLine();
-
-            Console.WriteLine("What's your height? (Please specify in cm)");
-            string personHeight = Console.ReadLine();
-
             Console.WriteLine("What's the day of week? (Monday-Sunday)");
             string dayOfWeek = Console.ReadLine();
             Enum.TryParse(dayOfWeek, out dayOfWeekEnum);
 
-            Int32.TryParse(personHeight, out personHeightcm);
-
-            Kid tom = new Kid(personName, personHeightcm, 1);
-
-            bool IsBatmanOpened(Day day)
+            //switch(dayOfWeekEnum)
+            //{
+            //    case Day.Sunday:
+            //        Console.WriteLine("Closed");
+            //        break;
+            //}
+            if (dayOfWeekEnum == Day.Sunday)
             {
-                if (day == Day.Monday || day == Day.Wednesday || day == Day.Friday)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                Console.WriteLine("Dear {0}, Attraction is closed today", personName);
+                Console.ReadLine();
             }
-
-            bool IsSwanOpened(Day day)
+            else
             {
-                if(day == Day.Tuesday || day == Day.Wednesday || day == Day.Thursday)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+                Console.WriteLine("Your gender?");
+                string personGender = Console.ReadLine();
 
-            bool IsPonyOpened(Day day)
-            {
-                if(day != Day.Sunday)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+                Console.WriteLine("What's your height? (Please specify in cm)");
+                string personHeight = Console.ReadLine();
 
-                bool IsBatman(int h)
+                Int32.TryParse(personHeight, out personHeightcm);
+
+                Kid tom = new Kid(personName, personHeightcm, 1);
+
+                bool IsBatmanOpened(Day day)
                 {
-                    if (h > 150)
+                    if (day == Day.Monday || day == Day.Wednesday || day == Day.Friday)
                     {
                         return true;
                     }
@@ -88,15 +65,78 @@ namespace Lesson2
                         return false;
                     }
                 }
-                bool isBatman = IsBatman(personHeightcm);
-                bool isBatmanOpened = IsBatmanOpened(dayOfWeekEnum);
 
-                if (personGender == "Male" && isBatman && isBatmanOpened)
+                bool IsSwanOpened(Day day)
                 {
-                    Console.WriteLine("child {0} is able to attend Batman", personName);
+                    if (day == Day.Tuesday || day == Day.Wednesday || day == Day.Thursday)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                bool IsPonyOpened(Day day)
+                {
+                    if (day != Day.Sunday)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                bool IsBatmanAllowed(string g, int h)
+                {
+                    if (g == "Male" && h > 150)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                bool IsSwanAllowed(string g, int h)
+                {
+                    if ((g == "Female" && h >= 120 && h < 140) || (g == "Male" && h < 140))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+
+                bool isBatmanAllowed = IsBatmanAllowed(personGender, personHeightcm);
+                bool isSwanAllowed = IsSwanAllowed(personGender, personHeightcm);
+                bool isBatmanOpened = IsBatmanOpened(dayOfWeekEnum);
+                bool isSwanOpened = IsSwanOpened(dayOfWeekEnum);
+                bool isPonyOpened = IsPonyOpened(dayOfWeekEnum);
+
+                if (isBatmanAllowed && isBatmanOpened)
+                {
+                    Console.WriteLine("Child {0} is permitted to attend Batman", personName);
+                    Console.ReadLine();
+                }
+                else if (isSwanAllowed && isSwanOpened)
+                {
+                    Console.WriteLine("Child {0} is permitted to attend Swan", personName);
+                    Console.ReadLine();
+                }
+                else if (isPonyOpened)
+                {
+                    Console.WriteLine("Child {0} is permitted to attend Pony", personName);
                     Console.ReadLine();
                 }
             }
         }
+        }
 }
-//if (dayOfWeekEnum == Day.Monday || dayOfWeekEnum == Day.Wednesday || dayOfWeekEnum == Day.Friday)
