bf3a1e47696d06331fffb8f8d9a4906f26ccc2bf
Oleksandr Ohorodnik <persyak@ukr.net>
2019-09-28 16:38:42 +0300

    Commit piramide project to github

---
+++ Start_Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Start_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] myString;
+            myString = new string[5,5];
+            string k = "*";
+
+            for (int i=0; i<5; i++)
+            {
+                for (int j=0; j<=i; j++)
+                {
+                    myString[i, j] = k;
+                    Console.Write("{0}\t", myString[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
