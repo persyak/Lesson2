445e3cf8d80eebc7400180e986b18e9f37871d0b
Oleksandr Ohorodnik <persyak@ukr.net>
2019-11-22 08:09:46 +0200

    dded 1st task from lesson1

---
+++ OM_L1_T1_Array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OM_L1_T1_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
