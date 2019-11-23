26d9ba0b8b3aef09109340b027dddfb4fdaefe75
Oleksandr Ohorodnik <persyak@ukr.net>
2019-11-23 15:42:48 +0200

    Commit lesson1 task 1

--- OM_L1_T1_Array/Program.cs
+++ OM_L1_T1_Array/Program.cs
@@ -10,6 +10,29 @@ namespace OM_L1_T1_Array
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[ ] {1,2,3,4,5,6,7};
+
+            // Console.WriteLine(nums[1]);
+            foreach (int i in nums)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+            
+            int d = nums.Length;
+            int h = d/2;
+            int tempint;
+            for (int i=0; i<h; i++)
+            {
+                tempint = nums[i];
+                nums[i] = nums[d - i - 1];
+                nums[d - i - 1] = tempint;
+            }
+            foreach (int i in nums)
+            {
+                Console.Write(i);
+            }
+            Console.ReadLine();
         }
     }
 }
