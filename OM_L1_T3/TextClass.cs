e641b0ed6df95b562e0087d79661c001593e2d10
Oleksandr Ohorodnik <persyak@ukr.net>
2019-11-25 08:52:52 +0200

    OM_L1_T3

---
+++ OM_L1_T3/TextClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OM_L1_T3
+{
+    class TextClass
+    {
+
+    }
+}
