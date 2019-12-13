c372c5ca3f5388f70a8f4019fca9a0d5156dd306
aogorodnik <alexandr.ogorodnik@excelian.com>
2019-12-13 17:51:49 +0200

    commit

---
+++ L1_T3_SB_Custom/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_T3_SB_Custom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ L1_T3_SB_Custom/StringClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_T3_SB_Custom
+{
+    class StringClass
+    {
+        private string mystring;
+
+        public string MyString
+        {
+            get { return mystring; }
+            set { }
+        }
+    }
+}
